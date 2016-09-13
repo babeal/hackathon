daf730b68bd8f98a4f6d1f1aaf45e9086fbb2d14
Your Name <you@example.com>
2016-09-13 13:27:28 -0400

    hello

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
+var gpio = require('rpi-gpio');
+var buzzerPin = 12;
+var ledRedPin = 16;
+var ledGreenPin = 18;
+ 
+gpio.setup(ledRedPin, gpio.DIR_OUT, write);
+ 
+function write() {
+    gpio.write(ledRedPin, false, function(err) {
+        if (err) throw err;
+        console.log('Written to pin');
+    });
+}
 
-import { sayHello } from "./sayHello"
 
-sayHello();
+
+//import { sayHello } from "./sayHello"
+
+//sayHello();
--- src/sayHello.js
+++ src/sayHello.js
@@ -1,3 +1,12 @@
+var gpio = require('rpi-gpio');
+gpio.setup(7, gpio.DIR_OUT);
+console.log("setup");
+ 
 export function sayHello() {
     console.log("Hello");
+
+    gpio.write(7, true, function(err) {
+        if (err) throw err;
+        console.log('Written to pin');
+    });
 }
