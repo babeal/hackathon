3b582ea51c64df6308154c981ff83814d9a6e80c
BBeal <babeal@outlook.com>
2016-09-13 12:56:02 -0500

    changed random cat facts

--- src/getRandomCatFact.js
+++ src/getRandomCatFact.js
@@ -6,20 +6,6 @@ var options = {
     path: '/api/facts'
 };
 
-var callback = function(response) {
-    var str = '';
-
-    //another chunk of data has been recieved, so append it to `str`
-    response.on('data', function (chunk) {
-        str += chunk;
-    });
-
-    //the whole response has been recieved, so we just print it out here
-    response.on('end', function () {
-        console.log(str);
-    });
-};
-
 export function getRandomCatFact() {
     return new Promise(function (resolve, reject) {
         http.request(options, function(response) {
@@ -35,13 +21,7 @@ export function getRandomCatFact() {
                 resolve(JSON.parse(str).facts[0]);
             });
         }).end();
-
-        // get('http://www.google.com', function (err, res) {
-        //     if (err) reject(err);
-        //     else resolve(res);
-        // });
     });
-
 }
 
 
